5add0eb7b7f416240a8e56f13d38faa62b5cdb0c
erchan <erchan_wu@asgard.com.tw>
2023-06-20 13:30:08 +0800

    feat: proxy 設定調整

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: '/wis_api',
+  apiUrl: '/wis_cms_api',
   msalClientId: 'db5c1985-ec31-490b-b382-be7b852287aa',
   msalAuthority:
     'https://login.microsoftonline.com/b66c13b9-de95-44b1-9eab-3f8679cd2ded',
