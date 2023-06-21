9f63ae9dd0fe69c83ec1c7968bc519b5f4fd1ddc
李俊逸(Woody Li) <woody_li@asgard.com.tw>
2023-06-21 16:35:19 +0800

    feat:更換AAD位置

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   apiUrl: '/wis_cms_api',
-  msalClientId: 'db5c1985-ec31-490b-b382-be7b852287aa',
+  msalClientId: '2104fd96-ab6a-45fc-8bd9-e2aed0b328a4',
   msalAuthority:
-    'https://login.microsoftonline.com/b66c13b9-de95-44b1-9eab-3f8679cd2ded',
+    'https://login.microsoftonline.com/79c48ca2-6c7d-4d6c-af4f-cbd28222475c',
   msalRedirectUri: 'http://localhost:21001/wis_cms_fe/',
 };
