4885c1f0c35a5c334c7f08c0124d27b898d353d2
erchan <erchan_wu@asgard.com.tw>
2023-07-04 09:29:01 +0800

    feat: sit、uat 設定檔

---
+++ src/environments/environment.sit.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  apiUrl: '/wis_cms_api',
+  // msalClientId: '2104fd96-ab6a-45fc-8bd9-e2aed0b328a4',
+  // msalAuthority:
+  //   'https://login.microsoftonline.com/79c48ca2-6c7d-4d6c-af4f-cbd28222475c',
+  //quickstart
+  msalClientId: '0cb3e5a4-37c0-4693-8bb0-97bee1e3c508',
+  msalAuthority:
+    'https://login.microsoftonline.com/b66c13b9-de95-44b1-9eab-3f8679cd2ded',
+  msalRedirectUri: 'https://192.168.51.223:20202/wis_cms_fe/',
+};
